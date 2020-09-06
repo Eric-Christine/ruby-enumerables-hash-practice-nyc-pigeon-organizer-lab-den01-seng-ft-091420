531b3c364e1a1f67d321542ebc2497b6c6d5dd31
Eric Christine <ericjchristine@gmail.com>
2020-09-06 19:01:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,7 @@ def nyc_pigeon_organizer(data)
         names.each do |name|
           binding.pry
           if !final_array[name]
-            name
+            final_array[name] = {}
           end
             
         end 
